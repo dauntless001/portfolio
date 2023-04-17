b1bafcfe843f17c996bb42cc5ef1b8c879cf5731
dauntless <suntopmultimedia@gmail.com>
2023-04-17 11:55:54 +0100

    template setup

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,6 @@ app_name = 'base'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('about-me/', views.AboutMe.as_view(), name='about'),
+    path('talk-to-me/', views.ContactMe.as_view(), name='contact'),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,10 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class AboutMe(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactMe(TemplateView):
+    template_name = 'contact.html'
