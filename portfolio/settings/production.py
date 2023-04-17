658d9ff42fdcd494240d51302fd96fc3c7b5da2f
dauntless <suntopmultimedia@gmail.com>
2023-04-17 12:02:35 +0100

    production

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -1,19 +1,24 @@
 from portfolio.settings.base import *
 from portfolio.utils.settings import get_env_variable
+import os, dj_database_url
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = get_env_variable("DEBUG", "True")
+DEBUG = os.environ.get('DEBUG', '') == 'False'
+
+# Whitenoise Middleware
+MIDDLEWARE.insert(2, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES['default'] ={
-    'ENGINE': get_env_variable("DB_ENGINE"),
-    'NAME': get_env_variable("DB_NAME"),
-    'USER': get_env_variable("DB_USER"),
-    'PASSWORD': get_env_variable("DB_PASSWORD"),
-    'HOST': get_env_variable("DB_HOST"),
-    'PORT': ''
-}
+DATABASES['default'] = dj_database_url.parse(
+    os.environ.get('DATABASE_URL'), conn_max_age=600
+)
+
+
+CSRF_COOKIE_SECURE = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'assets'
